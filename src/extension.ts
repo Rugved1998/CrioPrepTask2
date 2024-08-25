87b0f345bafbae40ffbefb7c13775649bc60e669
Aradhika Ramesh <rugved.bmw@gmail.com>
2024-08-25 16:31:52 +0530

    ecstension.ts setup

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { getWebviewContent } from './webviewContent';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,6 +10,24 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "gitcommand" is now active!');
+
+	context.subscriptions.push(
+        vscode.commands.registerCommand('extension.showChat', () => {
+            const panel = vscode.window.createWebviewPanel(
+                'chatPanel',
+                'Git Command Assistant',
+                vscode.ViewColumn.One,
+                {
+                    enableScripts: true,
+                    localResourceRoots: [
+                        vscode.Uri.joinPath(context.extensionUri, 'media')
+                    ]
+                }
+            );
+			panel.webview.html = getWebviewContent(context, panel.webview);
+        })
+    );
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
